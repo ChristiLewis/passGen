cbe4d0bf1ed50042a3d1ca920c5aa68ccd938966
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-02-24 13:24:19 -0800

    input concatenated but still not 100%

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,22 @@
 
 var generateBtn = document.querySelector("#generate");
 
-var numbers = [
+var possibleArray = [];
+
+const numbers = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 ];
 
-var specialChar = [
+const specialChar = [
   '@', '%', '+', '|', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
 ];
 
-var lowerCasedChar = [
+const lowerCasedChar = [
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ];
 
-var upperCasedChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+const upperCasedChar = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ];
 
 //Here the user is immediately involved, inspired by Bamar https://stackoverflow.com/users/1491895/barmar
@@ -33,71 +36,63 @@ getName();
 function passwordGenerator() {
   alert("Let passGen take the #$%&! out of making a new password!");
 
-  var possibleArray = [' '];
-  var finalPass = [' '];
-
-  function passwordLength(n) {
-    if(n < 8 || n > 128) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
   //FIGURE OUT THE USER'S PREFERRED VALUES
   //COMBINE INPUT INTO AN ARRAY
 
-  var passwordLength = prompt("How long do you want your password to be? Please select a number between 8-128");
-  if (isNaN(passwordLength)) {
-    alert('Please choose a number between greater than seven but less than 129')
-    return null;
+  var promptLength = window.prompt("How long do you want your password to be? Please select a number between 8-128");
+  if (promptLength < 8 || promptLength > 128 || promptLength === "") {
+    alert('Please type in a number greater than (7) but less than (129).')
+    passwordGenerator();
   } else {
-    console.log(passwordLength);
+    console.log(promptLength);
 
     var confirmNumbers = confirm("Do you want to include numbers?");
     if (confirmNumbers === false) {
-      !numbers;
+      var possibleArray1 = [""];
     } else {
-      const possibleArray = numbers;
-      console.log(possibleArray);
+      possibleArray1 = (numbers);
     };
+    console.log(possibleArray1);
 
     var confirmUpper = confirm("Do you want to include uppercase letters?");
-    if (confirmUpper === true) {
-      const possibleArray2 = upperCasedChar;
-      console.log(possibleArray2);
+    if (confirmUpper === false) {
+      var possibleArray2 = [""];
     } else {
-      !upperCasedChar;
+      possibleArray2 = possibleArray1.concat(upperCasedChar);
     };
+    console.log(possibleArray2);
 
     var confirmLower = confirm("Do you want to include lowercase letters?");
-    if (confirmLower === true) {
-      const possibleArray3 = lowerCasedChar;
-      console.log(possibleArray3)
+    if (confirmLower === false) {
+      var possibleArray3 = [""];
     } else {
-      !lowerCasedChar
+      possibleArray3 = possibleArray2.concat(lowerCasedChar);
     };
+    console.log(possibleArray3);
 
     var confirmSpecial = confirm("Do you want to include special symbols?");
-    if (confirmSpecial === true) {
-      const possibleArray4 =specialChar;
-      console.log(possibleArray4)
+    if (confirmSpecial === false) {
+      var possibleArray4 = [""];
+      possibleArray4.concat(possibleArray1, possibleArray2, possibleArray3)
+        alert("Please choose at least one character type for your password")
+        passwordGenerator();
     } else {
-      !specialChar.concat(possibleArray);
+      possibleArray4 = possibleArray3.concat(specialChar)
     };
+    console.log(possibleArray4);
 
     function randomChar(possibleArray) {
-      var randomIndex = Math.floor(Math.random() * passwordLength);
+      var randomIndex = Math.floor(Math.random() * promptLength);
       var randomElement = possibleArray[randomIndex];
       console.log(randomElement);
       return randomElement;
     };
 
-    function possibleArray(passwordLength) {
+    function possibleArray() {
       for (let i = 0; i < passwordLength; i++) {
         const possibleChar = randomChar(possibleArray);
         finalPass.concat(possibleChar);
-        finalPass.join('possibleArray');
+        finalPass.join(possibleArray,possibleArray1, possibleArray2, possibleArray3, possibleArray4);
         const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
         document.body.style.backgroundColor = rndCol;
       };
